2f07552c4999b8951eba95c5a55eabd584f3eafb
Gregory Langlais <general@gregorylanglais.com>
2019-03-22 19:26:52 -0400

    add handler to write command

--- write/main.go
+++ write/main.go
@@ -1,1 +1,30 @@
 package write
+
+import (
+	"context"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+// Response is of type APIGatewayProxyResponse since we're leveraging the
+// AWS Lambda Proxy Request functionality (default behavior)
+//
+// https://serverless.com/framework/docs/providers/aws/events/apigateway/#lambda-proxy-integration
+type Response events.APIGatewayProxyResponse
+
+// Handler is our lambda handler invoked by the `lambda.Start` function call
+func Handler(ctx context.Context) (Response, error) {
+	resp := Response{
+		StatusCode:      301,
+		Headers: map[string]string{
+			"Location": "http://example.com",
+		},
+	}
+
+	return resp, nil
+}
+
+func main() {
+	lambda.Start(Handler)
+}
