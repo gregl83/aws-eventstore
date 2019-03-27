7f8a214b1f232d4d5de6fd165187afefec6b002a
Gregory Langlais <general@gregorylanglais.com>
2019-03-26 21:13:37 -0400

    add migrate command to run db migrations

--- Makefile
+++ Makefile
@@ -2,6 +2,7 @@
 
 build:
 	dep ensure -v
+	env GOOS=linux go build -ldflags="-s -w" -o bin/migrate migrate/main.go
 	env GOOS=linux go build -ldflags="-s -w" -o bin/write write/main.go
 	env GOOS=linux go build -ldflags="-s -w" -o bin/read read/main.go
 	sls sam export -o template.yml
---
+++ migrate/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/github"
+)
+
+// Response is of type APIGatewayProxyResponse since we're leveraging the
+// AWS Lambda Proxy Request functionality (default behavior)
+//
+// https://serverless.com/framework/docs/providers/aws/events/apigateway/#lambda-proxy-integration
+type Response events.APIGatewayProxyResponse
+
+// Handler is our lambda handler invoked by the `lambda.Start` function call
+func Handler(request events.APIGatewayProxyRequest) (Response, error) {
+	// todo write to event store
+
+	m, err := migrate.New(
+		"github://mattes:personal-access-token@mattes/migrate_test",
+		"postgres://localhost:5432/database?sslmode=enable")
+	m.Steps(2)
+
+	fmt.Println("Received body: ", request.Body)
+
+	return Response{Body: request.Body, StatusCode: 200}, nil
+}
+
+func main() {
+	lambda.Start(Handler)
+}
