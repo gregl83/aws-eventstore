5a7f2f4d917b9e5d39b6b23b957dcf53301a1ece
Gregory Langlais <general@gregorylanglais.com>
2019-04-13 15:14:37 -0400

    add aurora password generate to aws ssm

--- ssm.js
+++ ssm.js
@@ -1,11 +1,36 @@
 const AWS = require('aws-sdk');
+const password = require('generate-password');
+
+AWS.config.update({
+    region: 'us-east-1'
+});
 
 const ssm = new AWS.SecretsManager();
 
-module.exports.AURORA_PASSWORD = () => {
-    // todo generate password if doesn't exist
+const passwordConfig = {
+    length: 16,
+    numbers: true,
+    strict: true,
+};
 
+const secretConfig = secret => {
+    return {
+        Description: "EventStore in Aurora database secret created by serverless",
+        Name: "EVS_AURORA_PASSWORD",
+        SecretString: secret
+    };
+};
+
+module.exports.AURORA_PASSWORD = () => {
     return ssm.getSecretValue({SecretId: 'EVS_AURORA_PASSWORD'})
         .promise()
-        .then(secret => secret.SecretString);
+        .then(secret => secret.SecretString)
+        .catch(err => {
+            if (err.name === 'ResourceNotFoundException') {
+                const secret = password.generate(passwordConfig);
+                return ssm.createSecret(secretConfig(secret)).promise();
+            } else {
+                console.log(err);
+            }
+        });
 };
